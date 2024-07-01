fd069d27e52d7ee61202c6be33128405914c4e07
lopendres2 <antonmenshikov70@gmail.com>
2024-07-01 13:15:14 +0300

    'эмулятор'

---
+++ src/main/java/com/example/newMock/Model/RequestDTO.java
@@ -0,0 +1,21 @@
+package com.example.newMock.Model;
+
+import lombok.*;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class RequestDTO {
+
+    private String rqUID;
+    private String clientId;
+    private String account;
+    private String openDate;
+    private String closeDate;
+
+
+}
